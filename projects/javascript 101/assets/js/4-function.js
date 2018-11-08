7f9ff7646cd468fd298b88645ad041af2239b105
Emil Risberg <emilrisb@lycksele.se>
2018-11-08 12:48:19 +0100

    functions_beginning

---
+++ projects/javascript 101/assets/js/4-function.js
@@ -0,0 +1,15 @@
+function greet(person1, person2, person3, person4) {
+    console.log("Hej," + person1 + "!");
+    console.log("Hej," + person2 + "!");
+    console.log("Hej," + person3 + "!");
+    console.log("Hej," + person4 + "!");
+}
+greet("emil", "pablo", "risberg", "escobar");
+
+function square(x) {
+    return x * x;
+}
+console.log ("4 squared is " + square(4))
+
+var result = square(10);
+console.log(result);
