799f06849887a7a9402b7ed5f0506b93b17677e4
Emil Risberg <emilrisb@lycksele.se>
2018-12-06 13:56:38 +0100

    Added remove function, styled ToDo-app

--- projects/toDo-list/assets/js/todo.js
+++ projects/toDo-list/assets/js/todo.js
@@ -9,7 +9,7 @@ document.getElementsByTagName("button")[0].addEventListener("click", function(){
             }
             else {
                 for (var i = 0; i < list.length; i++)
-                console.log("[" + i + "] " + list[i]);
+                console.log("[" + Number(i+1) + "] " + list[i]);
             }
         }
         if (userInput == "add"){
@@ -24,6 +24,13 @@ document.getElementsByTagName("button")[0].addEventListener("click", function(){
                 list.push(userAdd);
                 console.log(userAdd + " was added to the list.");
             }
+        }
+        if (userInput == "remove"){
+            var userRemove = Number(prompt("What is the index of the item you would like to remove?"));
+            if (userRemove > -1) {
+                list.splice((userRemove-1), 1);
+              }
+            console.log(userRemove + " was removed from the list.");
         }
     }
 });
