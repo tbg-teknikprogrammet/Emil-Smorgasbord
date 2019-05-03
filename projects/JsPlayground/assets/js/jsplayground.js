bc59c2986ef9d05a1e2483c7d9dca82baed69486
Emil Risberg <emilrisb@lycksele.se>
2019-05-03 12:07:19 +0200

    Continued beaver game

--- projects/JsPlayground/assets/js/jsplayground.js
+++ projects/JsPlayground/assets/js/jsplayground.js
@@ -186,26 +186,171 @@ dmSwitch.addEventListener("click", function(){
 ****************************/
 
 var clickAmount = 0;
+var logAmount = 0;
 var clickAmountDisplay = document.getElementById("clickamountdisplay");
 var beaver = document.getElementById("beaverimg");
 var flyingLog = document.getElementsByClassName("flyinglog");
+var helmet = document.getElementById("helmet");
+var axe = document.getElementById("axe");
+var helmetOn = false;
+var gameStarted = false;
+var gameEnded = false;
+var dead = false;
+var axeOn = false;
 
-beaver.addEventListener("click", function(){
-    clickAmount++;
-    clickAmountDisplay.innerHTML = clickAmount;
-    if (clickAmount > 5) {
-        beaver.src = "assets/images/Beaver2.png";
-    }
-    if (clickAmount > 10) {
-        beaver.src = "assets/images/Beaver3.png";
-    }
-    if (clickAmount > 15) {
-        beaver.src = "assets/images/Beaver4.png";
-    }
-})
-beaver.addEventListener("mousedown", function(){
-    flyingLog[0].classList.add("highlog");
-})
-beaver.addEventListener("mouseup", function(){
-    flyingLog[0].classList.remove("highlog");
-})
+if (gameEnded == false) {
+    beaver.addEventListener("click", function(){
+        gameStarted = true;
+        if(dead==false){clickAmount++};
+        clickAmountDisplay.innerHTML = clickAmount;
+        //The beaver is wearing nothing
+        if(helmetOn == false && axeOn == false) {
+            if (clickAmount > 10 && clickAmount < 21) {
+                beaver.src = "assets/images/Beaver2.png";
+            }
+            else if (clickAmount > 20 && clickAmount < 31) {
+                beaver.src = "assets/images/Beaver3.png";
+            }
+            else if (clickAmount > 30 && clickAmount < 91) {
+                beaver.src == "assets/images/Beaver4.png";
+            }
+            if (clickAmount == 50 && helmetOn==false) {
+                alert("The beaver is getting tired and beaten up! Buy him a helmet!");
+            }
+            if (clickAmount == 75 && helmetOn==false) {
+                alert("He is begging, buy a helmet!");
+            }
+            else if (clickAmount > 90 && helmetOn==false) {
+                dead = true;
+                clickAmount = 0;
+                beaver.src = "assets/images/deadbeaver.png";
+                alert("You've killed the beaver. How cruel!")
+                gameEnded = true;
+            }
+        }
+        // The beaver is only wearing a helmet
+        else if(helmetOn==true && axeOn == false){
+            beaver.src = "assets/images/Beaverhelmet.png";
+            if (clickAmount > 40) {
+                beaver.src = "assets/images/Beaverhelmet2.png";
+            }
+            if (clickAmount > 80) {
+                beaver.src = "assets/images/Beaverhelmet3.png";
+            }
+            if (clickAmount > 120) {
+                beaver.src = "assets/images/Beaverhelmet4.png";
+            }
+        }
+        // The beaver is only using an axe
+        else if(axeOn==true && helmetOn == false){
+            clickAmount++;
+            beaver.src = "assets/images/Beaveraxe.png";
+            if (clickAmount > 10) {
+                beaver.src = "assets/images/Beaveraxe2.png";
+            }
+            if (clickAmount > 20) {
+                beaver.src = "assets/images/Beaveraxe3.png";
+            }
+            if (clickAmount > 30) {
+                beaver.src = "assets/images/Beaveraxe4.png";
+            }
+        }
+        // The beaver uses an axe and is wearing a helmet
+        else if(axeOn==true && helmetOn == true){
+            clickAmount++;
+            beaver.src = "assets/images/Beaverhelmetaxe.png";
+            if (clickAmount > 40) {
+                beaver.src = "assets/images/Beaverhelmetaxe2.png";
+            }
+            if (clickAmount > 80) {
+                beaver.src = "assets/images/Beaverhelmetaxe3.png";
+            }
+            if (clickAmount > 120) {
+                beaver.src = "assets/images/Beaverhelmetaxe4.png";
+            }
+        }
+    })
+    
+    // Flying log on click
+    beaver.addEventListener("mousedown", function(){
+        flyingLog[0].classList.add("highlog");
+    })
+    beaver.addEventListener("mouseup", function(){
+        flyingLog[0].classList.remove("highlog");
+    })
+    //Running a function when the user clicks the helmet icon
+    helmet.addEventListener("click", function(){
+        if(clickAmount>49 && axeOn == true) {
+            helmetOn = true;
+            clickAmount = clickAmount-50;
+            clickAmountDisplay.innerHTML = clickAmount;
+            beaver.src = "assets/images/Beaverhelmet.png";
+            if (clickAmount > 10) {
+                beaver.src = "assets/images/Beaverhelmet2.png";
+            }
+            if (clickAmount > 20) {
+                beaver.src = "assets/images/Beaverhelmet3.png";
+            }
+            if (clickAmount > 30) {
+                beaver.src = "assets/images/Beaverhelmet4.png";
+            }
+        }
+        else if(clickAmount>49 && axeOn == false) {
+            helmetOn = true;
+            clickAmount = clickAmount-50;
+            clickAmountDisplay.innerHTML = clickAmount;
+            beaver.src = "assets/images/Beaverhelmet.png";
+            if (clickAmount > 10) {
+                beaver.src = "assets/images/Beaverhelmet2.png";
+            }
+            if (clickAmount > 20) {
+                beaver.src = "assets/images/Beaverhelmet3.png";
+            }
+            if (clickAmount > 30) {
+                beaver.src = "assets/images/Beaverhelmet4.png";
+            }
+        }
+        else if(clickAmount<50 && gameStarted == true){
+            alert("You need at least 50 logs to buy a helmet");
+        }
+    })
+    //Running a function when the user clicks the axe icon
+    axe.addEventListener("click", function(){
+        if(clickAmount>74 && helmetOn == true) {
+            axeOn = true;
+            clickAmount = clickAmount-50;
+            clickAmountDisplay.innerHTML = clickAmount;
+            beaver.src = "assets/images/Beaverhelmetaxe.png";
+            if (clickAmount > 40) {
+                beaver.src = "assets/images/Beaverhelmetaxe2.png";
+            }
+            if (clickAmount > 80) {
+                beaver.src = "assets/images/Beaverhelmetaxe3.png";
+            }
+            if (clickAmount > 120) {
+                beaver.src = "assets/images/Beaverhelmetaxe4.png";
+            }
+        }
+        else if(clickAmount>74 && helmetOn == false) {
+            axeOn = true;
+            clickAmount = clickAmount-75;
+            clickAmountDisplay.innerHTML = clickAmount;
+            beaver.src = "assets/images/Beaveraxe.png";
+            if (clickAmount > 10) {
+                beaver.src = "assets/images/Beaveraxe2.png";
+            }
+            if (clickAmount > 20) {
+                beaver.src = "assets/images/Beaveraxe3.png";
+            }
+            if (clickAmount > 30) {
+                beaver.src = "assets/images/Beaveraxe4.png";
+            }
+        }
+        else if(clickAmount<75 && gameStarted == true){
+            alert("You need at least 75 logs to buy an axe");
+        }
+    })
+}
+else if (gameEnded == true){
+    alert("You have killed a hardworking beaver. How cruel!")
+}
